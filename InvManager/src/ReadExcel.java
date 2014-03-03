05423321c582792bb43d7009cdcbb56fe81a868f
Diego Vasquez <dvazval@gmail.com>
2014-03-03 14:37:37 -0600

    Update 1
    Version 1

---
+++ InvManager/src/ReadExcel.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+//import poi.hssf.usermodel.HSSFCell;
+//import poi.hssf.usermodel.HSSFRow;
+//import poi.hssf.usermodel.HSSFSheet;
+//import poi.hssf.usermodel.HSSFWorkbook;
+
+
+
+public class ReadExcel {
+	
+	
+	public void leerArchivo(String rutaArchivo){
+		
+		/*
+		 File excel =  new File ("C:/Users/Leah-Dina/Desktop/LogFile.xlsx");
+	     FileInputStream fis = new FileInputStream(excel);
+	     HSSFWorkbook wb = new HSSFWorkbook(fis);
+	     HSSFSheet ws = wb.getSheet("Input");
+
+	     int rowNum = ws.getLastRowNum() + 1;
+	     int colNum = ws.getRow(0).getLastCellNum();
+	     String [][] data = new String [rowNum] [colNum];
+
+	     for(int i = 0; i <rowNum; i++){
+	         HSSFRow row = ws.getRow(i);
+	             for (int j = 0; j < colNum; j++){
+	                 HSSFCell cell = row.getCell(j);
+	                 String value = cell.toString();
+	                 data[i][j] = value;
+	                 System.out.println ("the value is " + value);
+	             }
+	     }
+	     */
+		
+		
+		
+		
+	}
+	
+	
+	
+
+ }
+	
+	
+	
+}
---
+++ InvManager/src/com/example/invmanager/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.invmanager;
+
+import android.R.array;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        createSpinner(R.id.spinner1, R.array.Categorias);
+     
+        
+    }
+    
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    
+    public void busquedaResultados(View view){
+    	// Buscar en los excel 
+    	
+    	
+    }
+    
+    
+    public void createSpinner(int idSpinner, int idArregloSpinner){
+    	Spinner spinner = (Spinner) findViewById(idSpinner);
+	    // Crear un ArrayAdapter utilizando el string array seleccionado y el layout por default
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+	     idArregloSpinner, android.R.layout.simple_spinner_item);
+	    // Especificar el layout a utilizar cuando la lista de opciones aparece 
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    // Aplicar el adaptador al Spinner
+	    spinner.setAdapter(adapter);
+    }
+    
+}
